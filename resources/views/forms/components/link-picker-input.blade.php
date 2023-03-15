26c5d5f704888ceb252537812944084c6053efd8
AngryMoustache <sander.vandamme11@gmail.com>
2023-03-15 15:37:00 +0100

    Fe stuffs

--- resources/views/forms/components/link-picker-input.blade.php
+++ resources/views/forms/components/link-picker-input.blade.php
@@ -64,13 +64,9 @@
                 </div>
             @else
                 @if (! $isDisabled())
-                    <button
-                        type="button"
-                        class="filament-button filament-button-size-sm inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset min-h-[2rem] px-3 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700"
-                        x-on:click.prevent="openPicker()"
-                    >
+                    <x-filament::button x-on:click.prevent="openPicker()">
                         {{ __('filament-link-picker.select link') }}
-                    </button>
+                    </x-filament::button>
                 @else
                     <p>{{ __('filament-link-picker.no link selected') }}</p>
                 @endif
