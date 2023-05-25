5d331b9136b5074a32fb43a06321e38fe69eac71
AngryMoustache <AngryMoustache@users.noreply.github.com>
2023-05-25 11:02:25 +0000

    Fix styling

--- lang/en/picker.php
+++ lang/en/picker.php
@@ -1,8 +1,8 @@
 <?php
 
-return array (
-  'chosen route' => 'Pick a link',
-  'choose a route' => 'Select link',
-  'confirm link' => 'Confirm selection',
-  'cancel pick' => 'Cancel',
-);
+return [
+    'chosen route' => 'Pick a link',
+    'choose a route' => 'Select link',
+    'confirm link' => 'Confirm selection',
+    'cancel pick' => 'Cancel',
+];
