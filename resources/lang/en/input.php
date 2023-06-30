fad2a50f932fa452d51a2f2378031f51d72e1ac9
Sander Van Damme <sander.vandamme@onlyhumans.com>
2023-06-30 07:56:38 +0000

    Fixed defult translations

--- resources/lang/en/input.php
+++ resources/lang/en/input.php
@@ -4,4 +4,7 @@ return [
     'route not found' => 'That route could not be found',
     'select link' => 'Selecting link',
     'no link selected' => 'No link has been selected',
+    'selected link' => 'Selected link',
+    'selected parameters' => 'Selected parameters',
+    'selected open in new tab' => 'Opens in a new tab',
 ];
