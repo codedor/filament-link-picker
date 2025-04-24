c9e5a49cd95494bff5cd7fedec2ab13cca4309d5
Jyrki De Neve <jyrki.deneve@gmail.com>
2025-04-24 22:04:59 +0300

    Add Dutch translations

--- resources/lang/en/input.php
+++ resources/lang/en/input.php
@@ -1,12 +1,11 @@
 <?php
 
 return [
-    'select link' => 'Selecting link',
+    'select link' => 'Select link',
     'selected link' => 'Selected link',
     'selected parameters' => 'Selected parameters',
     'selected open in new tab' => 'Opens in a new tab',
     'edit link' => 'Edit link',
-    'select link' => 'Select link',
     'remove link' => 'Remove link',
     'route label' => 'Route',
     'new tab label' => 'Open in new tab',
